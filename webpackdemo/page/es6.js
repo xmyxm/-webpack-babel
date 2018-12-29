2a4ea96b1888afbbd7c622909f12a48205713c54
chenxuanfeng <chenxuanfeng@meituan.com>
2018-12-29 20:07:13 +0800

    add

--- webpackdemo/page/es6.js
+++ webpackdemo/page/es6.js
@@ -1,3 +1,45 @@
+function timeout(num) {
+    return new Promise(resolve => {
+        setTimeout(function() { resolve() }, num)
+    })
+}
+console.log('start: ' + Date.now())
+timeout().then(console.log('end: ' + Date.now()))
+
+
+function* fetchJson() {
+    yield new Promise(resolve => {
+        fetch({ headers: { 'Content-Type': 'application/json' } }).then(res => {
+            if (res.ok) {
+                response.json().then(
+                    json => resolve({
+                        code: 200,
+                        data: json
+                    })
+                )
+            }
+        })
+    })
+    yield new Promise(resolve => {
+        fetch({ headers: { 'Content-Type': 'application/json' } }).then(res => {
+            if (res.ok) {
+                response.json().then(
+                    json => resolve({
+                        code: 200,
+                        data: json
+                    })
+                )
+            }
+        })
+    })
+}
+
+function includesTest() {
+    var arry = [1, 2, 3]
+    console.log(arry.includes(1))
+}
+
+
 class Event {
     constructor(prfx) {
         this.prfx = prfx || ''
